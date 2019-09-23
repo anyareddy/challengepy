c15b226f50b3e4489adb54e155453ad331fb589c
anyareddy <47282636+anyareddy@users.noreply.github.com>
2019-09-23 19:02:23 -0400

    Update index.py

--- index.py
+++ index.py
@@ -7,8 +7,10 @@ app = Flask(__name__)
 class Club:
     #List of all clubs created
     all_clubs = []
+    #List of all club names
     all_names = []
 
+    #Intialize with a club name, description, list of tags, number of facvorites, and user emails that have joined the club
     def __init__(self, name, description, tags, favorites, users):
         self.name = name
         self.description = description
@@ -21,6 +23,7 @@ class Club:
     def increaseFavorites(self):
         self.favorites += 1
 
+    #Gets the club info that needs to be posted on api/clubs - club names and number of favorites
     def clubInfo(self):
         all_info = []
         for club in Club.all_clubs:
@@ -31,14 +34,17 @@ class Club:
             all_info.append(info)
         return all_info
 
+    #Adds new user emails that join a club
     def updateUser(self, user):
         self.users.append(user)
 
 
 class User:
-    #List of all clubs created
+    #List of all users created
     all_users = []
+    #List of all usernames created
     all_usernames = []
+    #Intialize with a name, username, email, grad year, club names that they've favorited, a password, and clubs that they've joined
     def __init__(self, name, username, year, email, favorites, password, clubs):
         self.name = name
         self.username = username
@@ -50,6 +56,7 @@ class User:
         User.all_users.append(self)
         User.all_usernames.append(self.username)
 
+    #User info that gets put on their profile - excludes password
     def userInfo(self):
         user = {
             'name': self.name,
@@ -67,13 +74,14 @@ class User:
     def updateClubs(self, club):
         self.clubs = self.clubs.append(club)
 
+#Uses scraper to create all the club objects
 def createClubs():
     soup = soupify(get_clubs_html())
     club_soups = get_clubs(soup)
     for x in range(len(club_soups)):
         Club(get_club_name(club_soups[x]), get_club_description(club_soups[x]), get_club_tags(club_soups[x]), 0, [])
 
-
+#This code was used for testing purposes and to create Jennifer
 createClubs()
 user1 = User("Jennifer", "jen", 2022, "jen@seas.upenn.edu", [], "jenspass", ["Penn Labs", "Penn Program for Potential Procrastinators"])
 print(user1.clubs)
@@ -83,6 +91,8 @@ club.updateUser("jen@seas.upenn.edu")
 club2 = Club.all_clubs[2]
 club2.updateUser("jen@seas.upenn.edu")
 print(club2.users)
+#
+
 
 @app.route('/')
 def main():
@@ -96,15 +106,18 @@ def api():
 def get_clubs():
     if request.method == 'POST':
         count = 0
+        #First checks if the club sent in the request exists and updates the club info
         for clubs in Club.all_clubs:
             if clubs.name == request.json['name']:
                 count = 1
                 club = clubs
                 clubs.description = request.json['description']
                 clubs.tags = request.json['tags']
+        #If the club does not exist then we create a new club that is also added to existing club page
         if count == 0:
             club = Club(request.json['name'], request.json['description'], request.json['tags'], 0, []).__dict__
         return jsonify({'clubs': club})
+    #If it's a GET request then we just show all the club names and the number of favorites for each
     else:
         club = Club.all_clubs[0]
         return jsonify({'clubs' : club.clubInfo()}) #shows all club names only
@@ -129,6 +142,9 @@ def update_favorites():
     club.increaseFavorites()
     return jsonify({'clubs': club}), 201
 
+#Allows a user to join a club if they want - this adds their email to the club users variable 
+#(Note that I made joining a club for the purpose of allowing people who are interested to conatct you / for future could
+#allow you to join the listserv, etc.)
 @app.route('/api/joinClub', methods = ['POST'])
 def update_clubs():
     user = request.json['user']
@@ -137,6 +153,8 @@ def update_clubs():
     club.updateUser(user.email)
     return jsonify({'clubs': club}), 201
 
+#Allows users to get all the emails of members of a certain club so that they can contact indiviuals if they are interested
+#The url includes the club name
 @app.route('/api/members/<club>', methods = ['GET'])
 def getMembers(club):
     members = []
